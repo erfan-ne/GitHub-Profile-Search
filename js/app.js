7dc353df83e8d357b100a18d04582f82423ad20d
erfan-ne <erfan.neshati@gmail.com>
2025-07-05 20:11:20 +0330

    fit(app.js): add Fetch API from github

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 //* https://api.github.com/users/[USERNAME]
 
+const searchInput = document.querySelector("#search-input")
+const searchBtn = document.querySelector("#search-button")
+const userCard = document.querySelector("#user-card")
+
+
+const searchUser = () => {
+  fetch(`https://api.github.com/users/${searchInput.value}`)
+    .then(response => response.json())
+    .then(gitHubUser =>{
+
+    })
+  
+}
+
+searchBtn.addEventListener("click", searchUser)
